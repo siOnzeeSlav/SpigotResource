ad8e39e386c7c0fcaa89ea991a11fba26ac96845
Jan Tržický <sionzee@sionzee.cz>
2015-08-19 21:03:46 +0200

    Added join and catched exception [startFrom]

--- src/cz/sionzee/spigotresource/utils/Array.java
+++ src/cz/sionzee/spigotresource/utils/Array.java
@@ -9,15 +9,40 @@ package cz.sionzee.spigotresource.utils;
 
 public class Array {
 
-    public static <T> T[] startFrom(T[] array, Class<T> clazz, int index) throws Exception {
+    /**
+     * Create new array and set index zero {index} of {array}
+     * @param array Source
+     * @param clazz Array Type? .class
+     * @param index From which it start?
+     * @param <T> Array Type?
+     * @return new Array
+     */
+    public static <T> T[] startFrom(T[] array, Class<T> clazz, int index) {
         if (index >= array.length)
-            throw new Exception("Index cannot be higher or equal to size of array");
+            try {
+                throw new Exception("Index cannot be higher or equal to size of array");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         int size = array.length - index;
         T[] result = (T[]) java.lang.reflect.Array.newInstance(clazz, size);
         for (int i = 0; i < size; i++) {
             result[i] = array[index + i];
-            //System.out.println("[" + i + "] = " + result[i]);
+        }
+        return result;
+    }
+
+    /**
+     * Join array values by {join} (ignore last index)
+     * @param array [The, Word]
+     * @param join " ,.- "
+     * @return (The ,.- Word)
+     */
+    public static String join(String[] array, String join) {
+        String result = "";
+        for(int i = 0; i < array.length; i++) {
+            result += array[i] + (i == array.length? "": join);
         }
         return result;
     }
