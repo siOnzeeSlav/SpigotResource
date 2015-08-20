39295764c9d9da1f2f7621ade702aaf9a25f8ebb
Jan Tržický <sionzee@sionzee.cz>
2015-08-20 19:39:04 +0200

    Added Debug

---
+++ src/cz/sionzee/spigotresource/debug/Debug.java
@@ -0,0 +1,48 @@
+/*
+ * [SpigotResource]
+ * Created by sionzee.
+ * All rights reserved to siOnzee.cz 2015
+ * Repository: https://github.com/siOnzeeSlav/SpigotResource
+ */
+
+package cz.sionzee.spigotresource.debug;
+
+import cz.sionzee.spigotresource.utils.Formater;
+import org.bukkit.Bukkit;
+
+import java.util.logging.Level;
+
+public class Debug {
+
+    /**
+     * Log via {@link Formater#format(Object, String, Object...)}
+     * @param instanceOfClass Object
+     * @param format String
+     * @param args Object...
+     */
+    public static void log(Object instanceOfClass, String format, Object... args) {
+        Bukkit.getLogger().log(Level.INFO, Formater.format(instanceOfClass, format, args));
+    }
+
+    /**
+     * Log error via {@link Formater#format(Object, String, Object...)}
+     * @param instanceOfClass Object
+     * @param format String
+     * @param args Object...
+     */
+    public static void error(Object instanceOfClass, String format, Object... args) {
+        Bukkit.getLogger().log(Level.SEVERE, Formater.format(instanceOfClass, format, args));
+    }
+
+    /**
+     * Log error via {@link Formater#format(Exception, Object, String, Object...)}
+     * @param exception Exception
+     * @param instance Object
+     * @param format String
+     * @param args Object...
+     */
+    public static void error(Exception exception, Object instance, String format, Object... args) {
+        Bukkit.getLogger().log(Level.SEVERE, Formater.format(exception, instance, format, args));
+    }
+
+}
